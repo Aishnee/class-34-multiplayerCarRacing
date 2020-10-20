fce925dc05835b10b11ca848851cdffc28b52552
Aishnee <meetrini@gmail.com>
2020-10-20 18:58:51 +0530

    class-34

--- sketch.js
+++ sketch.js
@@ -1,29 +1,39 @@
-var ball;
+var ball,ballPosition,database,position;
 
 function setup(){
+    database=firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    database.ref("ball/position").on("value",readPosition);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+   database.ref("ball/position").set({
+       x:ballPosition.x+x,
+       y:ballPosition.y+y
+    })
+}
+
+function readPosition(data){
+ballPosition=data.val();
+ball.x=ballPosition.x;
+ball.y=ballPosition.y;
 }
